6e67c423209d2dac83a5346a65fe102ca5b8a2f1
kristjanjansen <kristjan.jansen@gmail.com>
2020-01-07 23:24:47 +0100

    cleanup

--- src/index.js
+++ src/index.js
@@ -12,32 +12,9 @@ Vue.prototype.$global = new Vue({ data: { state: {} } });
 
 new Vue({
   setup() {
-    const { editorNode, content } = useMonaco("yea");
-    // const editorNode = ref(null);
-    // const content = ref("<f-scene></f-scene>");
-    // onMounted(() => {
-    //   monaco.languages.registerCompletionItemProvider("html", {
-    //     provideCompletionItems
-    //   });
-    //   monaco.languages.registerHoverProvider("html", { provideHover });
-    //   const editor = monaco.editor.create(editorNode.value, {
-    //     value: content.value,
-    //     language: "html",
-    //     theme: "vs-dark",
-    //     fontSize: "14px",
-    //     wordWrap: "wordWrapColumn",
-    //     wordWrapColumn: 65,
-    //     minimap: {
-    //       enabled: false
-    //     }
-    //   });
-    //   const model = editor.getModel();
-    //   model.updateOptions({ tabSize: 2 });
-
-    //   editor.onDidChangeModelContent(e => {
-    //     content.value = editor.getValue();
-    //   });
-    // });
+    const { editorNode, content } = useMonaco(
+      "<f-scene grid>\n  <f-circle/>\n</f-scene>"
+    );
     return { editorNode, content };
   },
   template: `
